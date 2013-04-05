6faac2c7d697ff1995974e0e2520d20e9a44b78e
sunxiaoliang <418299945@qq.com>
2013-04-05 17:19:54 +0800

    sxl

---
+++ src/main/java/org/lsq/action/MessageSettingsAction.java
@@ -0,0 +1,11 @@
+package org.lsq.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class MessageSettingsAction extends ActionSupport {
+	public String execute() {
+		System.out.println("MessageSettings Action ……");
+		return "hello";
+
+	}
+}
