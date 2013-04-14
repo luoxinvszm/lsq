7053f77b0d5ee11296b49b4468973579900c1c0a
sunxiaoliang <418299945@qq.com>
2013-04-14 12:01:26 +0800

    messagesettings

--- src/main/java/org/lsq/action/MessageSettingsAction.java
+++ src/main/java/org/lsq/action/MessageSettingsAction.java
@@ -1,5 +1,7 @@
 package org.lsq.action;
 
+import org.lsq.service.IMessageSettingsService;
+
 import com.opensymphony.xwork2.ActionSupport;
 
 public class MessageSettingsAction extends ActionSupport {
@@ -7,10 +9,38 @@ public class MessageSettingsAction extends ActionSupport {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
+	private IMessageSettingsService messageSettingsService;
+	private String method;
+
+	public String getMethod() {
+		return method;
+	}
+
+	public void setMethod(String method) {
+		this.method = method;
+	}
+
+	public IMessageSettingsService getMessageSettingsService() {
+		return messageSettingsService;
+	}
+
+	public void setMessageSettingsService(
+			IMessageSettingsService messageSettingsService) {
+		this.messageSettingsService = messageSettingsService;
+	}
+
+	public String InsertMessageSettings() {
+		System.out.println("……");
+		System.out.println(method);
+		return INPUT;
+	}
 
 	public String execute() {
 		System.out.println("MessageSettings Action ……");
-		return "hello";
+		System.out.println(method);
+		//messageSettingsService.InsertMessageSettings("SendMsgNum", 2);
+		return INPUT;
 
 	}
+
 }
