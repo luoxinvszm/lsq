e6da2f93dbeb9c5d449077f82dd8739201b084f7
jason <1297197323@qq.com>
2013-04-20 08:43:48 +0800

    yzp

--- src/main/java/org/lsq/util/IdBuilder.java
+++ src/main/java/org/lsq/util/IdBuilder.java
@@ -9,15 +9,12 @@ import java.util.concurrent.locks.ReentrantLock;
  * 
 */
 public class IdBuilder {
-	//例子：生成Id
-//	public static void main(String[] args) {
-
-	//	System.out.println(IdBuilder.getNewId());
-
-	//	IdBuilder getId = new IdBuilder();
-	//	System.out.println(getId.getNewId());
+	//例子：生成Id并在控制台输出
+	public static void main(String[] args) {
+		IdBuilder getId = new IdBuilder();
+		System.out.println(getId.getNewId());
 		
-	//}
+	}
 	
 	
 	/**
