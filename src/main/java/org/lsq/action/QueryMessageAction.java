2f58a33bd2c56f68678094eb0f81549a356065c2
jason <1297197323@qq.com>
2013-04-20 14:02:33 +0800

    yzp

--- src/main/java/org/lsq/action/QueryMessageAction.java
+++ src/main/java/org/lsq/action/QueryMessageAction.java
@@ -75,18 +75,19 @@ public class QueryMessageAction extends ActionSupport {
 
 		System.out.println("qureymessageaction starting····" );
 
-		messageList = qureyMessageService.checkQuery(publisherPhone,publishTime,msgStatus);
-		for (Message m : messageList) {
-			System.out.println(m.getMsgId());
-			System.out.println(m.getMsgTypeId());
-			System.out.println(m.getPublisherName());
-			System.out.println(m.getPublisherPhone());
-			System.out.println(m.getMsgRemark());
-			System.out.println(m.getPublishTime());
-			System.out.println(m.getMsgConctent());
-			System.out.println(m.getMsgStatus());
+//		messageList = qureyMessageService.checkQuery(publisherPhone,publishTime,msgStatus);
+//		for (Message m : messageList) {
+//			System.out.println(m.getMsgId());
+//			System.out.println(m.getMsgTypeId());
+//			System.out.println(m.getPublisherName());
+//			System.out.println(m.getPublisherPhone());
+//			System.out.println(m.getMsgRemark());
+//			System.out.println(m.getPublishTime());
+//			System.out.println(m.getMsgConctent());
+//			System.out.println(m.getMsgStatus());
+			System.out.println("publishTime="+publishTime);
 			System.out.println("***************************************");
-		}
+		//}
 		return SUCCESS;
 
 	}
